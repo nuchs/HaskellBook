c474315f1128803bd0a0d3e4e53d37efc18083d3
nuchs <sjbrown@live.co.uk>
2017-12-19 16:37:29 +0000

    Finished chapter nine exercises

--- src/c9.hs
+++ src/c9.hs
@@ -71,3 +71,69 @@ shout (x:xs) = toUpper x : shout xs
 
 initials :: String -> Char
 initials = toUpper . head 
+
+-- 1
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (True:xs) = True
+myOr (_:xs) = myOr xs
+
+-- 2
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny _ [] = False
+myAny f (x:xs)
+  | f x == True = True
+  | otherwise = myAny f xs
+
+-- 3
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ [] = False
+myElem x (y:ys)
+  | x == y = True
+  | otherwise = myElem x ys
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' x ys = myAny (\a -> a == x) ys
+
+-- 4
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse xs = acc [] xs
+  where acc ys [] = ys
+        acc ys (x:xs) = acc (x:ys) xs
+
+-- 5
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x:xs) = x ++ squish xs
+
+-- 6
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f (x:xs) = f x ++ squishMap f xs
+
+-- 7
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+-- 8
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs) = findMax f x xs
+  where findMax _ m [] = m
+        findMax f m (x:xs) = let currMax = if (f x m == GT) then x else m
+                             in findMax f currMax xs
+
+-- 9
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x:xs) = findMin f x xs
+  where findMin _ m [] = m
+        findMin f m (x:xs) = let currMin = if (f x m == LT) then x else m
+                             in findMin f currMin xs
+
+-- 10
+myMaximum :: (Ord a) => [a] -> a
+myMaximum = myMaximumBy compare
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum = myMinimumBy compare
--- src/cipher.hs
+++ src/cipher.hs
@@ -2,3 +2,21 @@ module Cipher where
 
 import Data.Char
 
+ceaser :: Int -> String -> String
+ceaser = shiftString
+
+unCeaser :: Int -> String -> String
+unCeaser x = shiftString (-x)
+
+shiftString :: Int -> String -> String
+shiftString  s xs = map shifter xs
+  where shifter = shiftChar s
+
+shiftChar :: Int -> Char -> Char
+shiftChar x c = newValue
+  where base = ord 'a'
+        ordinal = ord c - base
+        shiftedOrdinal = (ordinal + x) `mod` 26
+        newValue = chr (base + shiftedOrdinal)
+
+
